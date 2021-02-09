f1083abe11aaf3f3e734cda275a5aeedfd2a5b2d
cgeopapa <cgeocodgod@gmail.com>
2021-02-09 13:36:04 +0200

    Added graph to assetCartography

--- src/view/AssetCartographyView.tsx
+++ src/view/AssetCartographyView.tsx
@@ -11,11 +11,15 @@ import { Dropdown } from 'primereact/dropdown';
 import { Button } from 'primereact/button';
 import { OverlayPanel } from 'primereact/overlaypanel';
 import { Tooltip } from 'primereact/tooltip';
+import { TabView,TabPanel } from 'primereact/tabview';
+import { Graph } from "react-d3-graph";
 import AssetController from '../controller/AssetController';
 import CategoryController from '../controller/CategoryController';
 import Owner from '../model/Owner';
 import OwnerContrller from '../controller/OwnerController';
 import TopMenuView from './TopMenuView';
+import Asset from '../model/Asset';
+import { link } from 'fs';
 
 export default class AssetCartographyView extends Component{
     private assetsController = new AssetController();
@@ -33,7 +37,9 @@ export default class AssetCartographyView extends Component{
             categories: null,
             owners: null,
             editingOwner: new Owner(),
-            loading: true
+            loading: true,
+            data: {},
+            config: {},
         }
     }
 
@@ -171,19 +177,56 @@ export default class AssetCartographyView extends Component{
                 <div className="customClassName" data-pr-tooltip="Located/Installed/Saved at..">Location</div>
             </div>
         )
+        const prepareGraph = (e: any) => {
+            const nodes = [];
+            const links = [];
+            for(let asset of this.state.assets){
+                if(asset.name){
+                    nodes.push({id: asset.name});
+                }
+                if(asset.location){
+                    links.push({source: asset.name, target: asset.location});
+                }
+            }
+            const data = {nodes: nodes, links: links};
+
+            const myConfig = {
+                nodeHighlightBehavior: true,
+                height: 640,
+                width: 2048,
+                node: {
+                  color: "lightgreen",
+                  size: 120,
+                  highlightStrokeColor: "darkgreen",
+                },
+                link: {
+                  highlightColor: "lightblue",
+                },
+            };
+            this.setState({activeIndex: e.index, data: data, config: myConfig});
+        }
         return (
             <div style={{width:"100%", height:"100%"}} className="p-card">
                 <TopMenuView/>
-                <DataTable loading={this.state.loading} value={this.state.assets} editMode="row" sortMode="multiple" className="p-datatable-striped"
-                resizableColumns columnResizeMode="fit" header={header}
-                onRowEditInit={(e) => this.onRowEditInit(e)} onRowEditCancel={(e) => this.onRowEditCancel(e)} onRowEditSave={(e) => this.onRowEditSave(e)}>
-                    <Column field="name" header="Asset Name" editor={(e) => this.inputTextEditor(e.rowData, "name")}/>
-                    <Column field="description" header="Description" editor={(e) => this.inputTextEditor(e.rowData, "description")}/>
-                    <Column field="category" header="Category" body={this.categoryTemplate} editor={(e) => this.inputCategoryEditor(e.rowData)}/>
-                    <Column field="owner" header="Owner" body={this.ownerTemplate} editor={(e) => this.inputOwnerEditor(e.rowData)}/>
-                    <Column field="location" header={locationHeader} editor={(e) => this.inputLocationEditor(e.rowData)}/>
-                    <Column rowEditor headerStyle={{ width: '7rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
-                </DataTable>
+                <TabView activeIndex={this.state.activeIndex} onTabChange={prepareGraph}>
+                    <TabPanel header="Table" leftIcon="pi pi-table">
+                        <DataTable loading={this.state.loading} value={this.state.assets} editMode="row" sortMode="multiple" className="p-datatable-striped"
+                        resizableColumns columnResizeMode="fit" header={header}
+                        onRowEditInit={(e) => this.onRowEditInit(e)} onRowEditCancel={(e) => this.onRowEditCancel(e)} onRowEditSave={(e) => this.onRowEditSave(e)}>
+                            <Column field="name" header="Asset Name" editor={(e) => this.inputTextEditor(e.rowData, "name")}/>
+                            <Column field="description" header="Description" editor={(e) => this.inputTextEditor(e.rowData, "description")}/>
+                            <Column field="category" header="Category" body={this.categoryTemplate} editor={(e) => this.inputCategoryEditor(e.rowData)}/>
+                            <Column field="owner" header="Owner" body={this.ownerTemplate} editor={(e) => this.inputOwnerEditor(e.rowData)}/>
+                            <Column field="location" header={locationHeader} editor={(e) => this.inputLocationEditor(e.rowData)}/>
+                            <Column rowEditor headerStyle={{ width: '7rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
+                        </DataTable>
+                    </TabPanel>
+                    <TabPanel header="Graph" leftIcon="pi pi-share-alt">
+                        <div>
+                            <Graph id="graph-id" data={this.state.data} config={this.state.config}/>
+                        </div>
+                    </TabPanel>
+                </TabView>
             </div>
         );
     }
